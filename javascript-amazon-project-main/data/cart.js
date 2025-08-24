6329ead1652b358beaef274f894bc75128dff4e2
jyotisingh <jyotisinghhw@gmail.com>
2025-08-25 01:16:17 +0530

    move checkout to js code

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,7 +1,17 @@
-export const cart=[];
+export const cart= JSON.parse(localStorage.getItem("cart"))||
+[{
+    productId:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity:1
+},{
+   productId:"15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity:2 
+}];
+
+export function saveCart(){
+localStorage.setItem("cart",JSON.stringify(cart));
+}
 export function addToCart(productId){
     let founditems="";
-
     cart.forEach((item)=>{
         if(item.productId===productId){
             founditems=item;
@@ -17,6 +27,7 @@ export function addToCart(productId){
         quantity:1
     }); 
     }
+    saveCart()
 }
 export function updateCartQuantity(){
       let cartQantity=0;
@@ -24,5 +35,8 @@ export function updateCartQuantity(){
     cartQantity+=item.quantity;
 
    });
+
    document.querySelector(".js-cart-quantity").innerHTML=cartQantity;
+   
+      saveCart()
 }
